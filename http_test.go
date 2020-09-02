9897213ecbd1526a4adbb456487534777f25aaa7
zwdgithub <zwdjobs@126.com>
2020-09-02 22:23:29 +0800

    custom client custom req client.post

--- http_test.go
+++ http_test.go
@@ -1,10 +1,30 @@
-package main
+package simple_http
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+	"time"
+)
+
+func custom(client *http.Client) {
+	client.Timeout = time.Nanosecond * 1
+	client.Transport = nil
+}
 
 func TestHttp(t *testing.T) {
 	h := NewHttpUtil()
-	content, err := h.Get("http://www.baidu.com", nil).Do().ResultContent()
+	content, err := h.Get("https://www.baidu.com", nil).
+		CustomClient(func(client *http.Client) {
+			client.Timeout = 1111
+		}).
+		CustomClient(custom).
+		Do().
+		RContent()
+
 	t.Log(content)
 	t.Log(err)
+}
+
+func TestHttp1(t *testing.T) {
+	t.Log("http1")
 }
